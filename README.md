8b6ff18e085c70717070da4ab977519f689891f4
Florian Fischer <ffischer@bloopark.de>
2014-12-19 10:16:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Section Separators
+==================
+
+- this Add-on provides the integration of two different styles to divide content sections
+- install the addon  via settings > modules
+- the selection of different separation styles is available in the "cusomize dropdown" of the Odoo website builder
+- warning: does not work with background color "darken" (oe_dark)
---
+++ __openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Modal Popup extention",
+
+    'summary': """
+    Provide different separator styles""",
+
+    'description': """
+    Provide different separator styles""",
+
+    'author': "bloopark systems GmbH & Co. KG",
+    'website': "http://www.bloopark.de",
+
+    'category': 'Modal',
+    'version': '1.0',
+
+    'depends': [
+        'base',
+        'website'
+    ],
+    'data': [
+        'views/snippets.xml'
+    ],
+
+    'demo': [
+    ],
+
+    'tests': [
+    ],
+}
